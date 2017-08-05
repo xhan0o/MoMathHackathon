120074b81bb4c255e528f3896e7014de104c7cd3
shan0o <shantanoopatel@gmail.com>
2017-08-05 19:37:25 -0400

    Answers work in axis

--- behs/xhn.js
+++ behs/xhn.js
@@ -19,6 +19,10 @@ var s
 var step=false;
 var num1;
 var num2;
+var answer;
+var axx;
+var ayy;
+var problemStatement;
 pb.setup = function (p) {
   /* this == pb.p5 == p */
   /* P5Behavior already calls createCanvas for us */
@@ -34,22 +38,50 @@ var j = 0;
 
 while (!step) {
 this.clear();
+//problem statement 
+var p1 = Math.random()*10;
+var p2 = Math.random()*10;
+
+num1 = p1.toFixed();
+num2 = p2.toFixed();
+answer = +num1 + +num2;
+console.log(answer);
+var prob1 = num1.toString();
+var prob2 = num2.toString();
+problemStatement = prob1.concat(" + ",prob2, " = ??");
+console.log(problemStatement)
+
+this.fill(256, 256, 256);
+
 while(j<10) {
 	
 var s = Math.random()*99;
 var yy = Math.random()*576;
 var xx = Math.random()*576;
+this.text(problemStatement,150,32);
 this.text(s.toFixed(), yy.toFixed(), xx.toFixed());
 this.fill(256, 256, 256);
 j++;
 }
+//print answer on board
+ayy = Math.random()*576;
+axx = Math.random()*576;
+this.text(answer,ayy.toFixed(), axx.toFixed());
+
+
 step = true;
 }
 for(var i = new Sensor.Index(); i; i = i.incr()){
     if (floor.sensors.get(i)){
         console.log('This sensor is enabled');
 		console.log(i.x*8,i.y*8);
-		step = false;
+		console.log(axx,ayy)
+		
+	if (axx < i.x*8 + 10 || axx > i.x*8 - 10) {
+    step = false;
+	} else {
+    step = true;
+	}
 		
     }
 }
